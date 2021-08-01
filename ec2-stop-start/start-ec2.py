c9b1c3cdf69cf7658f4fc7359ddce08d2fe7ec0b
Manzoor Ahamed <ahamedm@meredith.com>
2021-08-01 12:46:53 +0530

    ec2 start instances in all region

---
+++ ec2-stop-start/start-ec2.py
@@ -0,0 +1,34 @@
+#!/opt/anaconda3/bin/python
+
+## Module
+import boto3
+import argparse
+
+## Create the argparser
+parser = argparse.ArgumentParser()
+
+## Add the arguments
+parser.add_argument("profile_name",help="Profile Name of the account")
+parser.add_argument("region",help="Region Name")
+
+## Execute the parse_arg() method
+args = parser.parse_args()
+
+## Session Creation
+session = boto3.Session(profile_name=args.profile_name,region_name=args.region)
+#session = boto3.Session(profile_name=args.profile_name)
+ec2 = session.resource('ec2')
+
+## Collecting the region details
+regions = []
+for region in ec2.meta.client.describe_regions()['Regions']:
+    regions.append(region['RegionName'])
+
+
+for region in regions:
+    ec2 = session.resource("ec2",region_name=region)
+    ec2_filter = {"Name": "instance-state-name","Values": ["stopped"]}
+    instances = ec2.instances.filter(Filters=[ec2_filter])
+    for instance in instances:
+        instance.start()
+
